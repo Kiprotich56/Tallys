45df0cec9e41be655fe786c115710191fdcae627
Levy Kiprotich <cheruiyotlevy56@gmail.com>
2026-06-27 19:08:46 +0300

    Update main.tsx

--- artifacts/tallys/src/main.tsx
+++ artifacts/tallys/src/main.tsx
@@ -2,4 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+import { setBaseUrl } from "@workspace/api-client-react";
+
+// Replace with your Render backend URL
+setBaseUrl("https://tallys-ne1s.onrender.com");
+
 createRoot(document.getElementById("root")!).render(<App />);
