87a885ce02137b99c371858a7d425561cbf9de07
Levy Kiprotich <cheruiyotlevy56@gmail.com>
2026-06-30 14:17:43 +0300

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -30,7 +30,10 @@ app.use(
   }),
 );
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
